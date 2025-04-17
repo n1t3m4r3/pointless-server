ee511f7182c9a54aa467e9eee184a820338c63e1
n1t3m4r3 <140575445+n1t3m4r3@users.noreply.github.com>
2025-04-17 10:20:31 +0000

    final

--- result-check.py
+++ result-check.py
@@ -1,16 +1,18 @@
 import requests
 import time
 
-def check_jee_mains_result(url, webhook_url, interval=10):
-    """Checks the JEE Mains result website and notifies via Discord webhook when available."""
+def check_jee_mains_result(url, webhook_url, interval=30):
+    x1 = requests.get(url, timeout=7)
+    x2 = x1.text
     while True:
         try:
-            response = requests.get(url, timeout=5)
-            if response.status_code != 500:
+            y1 = requests.get(url, timeout=7)
+            y2 = y1.text
+            if x2 != y2:
                     notify_discord(webhook_url, "website change")
-                    time.sleep(3000)
+                    time.sleep(60)
             else:
-                print("Still showing 'Internal Server Error', retrying...")
+                print("no change")
         except requests.RequestException as e:
             print(f"Request failed: {e}, retrying...")
         
@@ -29,6 +31,6 @@ def notify_discord(webhook_url, message):
         print(f"Failed to send Discord notification: {e}")
 
 if __name__ == "__main__":
-    JEE_MAINS_URL = "https://jeemain.nta.nic.in/results-for-jeemain-2025-session-1/link"
-    DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/1338562729916760146/gTlZwdMtUr0aMzF0VDCrkNX9k-WYxvLU9cYEQMFaaOEfLlQBbLytj40eBQiuFS3hdTXy"
+    JEE_MAINS_URL = "https://jeemain.nta.nic.in/#1648447930282-deb48cc0-95ec"
+    DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/912965858198949918/LEjxLyA9OP51xgRYqSufxCDCTi-6zvOXmqfoUq0_YF0Gp68CcBEmzoAcJnA8ulpzfXYT"
     check_jee_mains_result(JEE_MAINS_URL, DISCORD_WEBHOOK_URL)
